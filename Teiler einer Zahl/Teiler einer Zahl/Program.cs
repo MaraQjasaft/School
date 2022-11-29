0049fbf0acc053e5d8aa4195077ee503382d6ae0
MaraQjasaft <116597622+MaraQjasaft@users.noreply.github.com>
2022-11-29 09:44:41 +0100

    Add files via upload

---
+++ Teiler einer Zahl/Teiler einer Zahl/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teiler_einer_Zahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl, ergebnis, teiler_anzahl = 1, zaehler = 1;
+            string antwort;
+
+            do
+            {
+                Console.WriteLine("Ausgabe der Teiler einer Zahl");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine();
+                Console.Write("Bitte eine ganze Zahl > 0 eigeben: ");
+                zahl = Convert.ToInt32(Console.ReadLine());
+
+
+
+                for (int i = 1; i < zahl + 1; i++)
+                {
+
+                    if (zahl % i == 0)
+                    {
+                        ergebnis = zahl / i;
+                        Console.WriteLine("Der " + teiler_anzahl + ". Teiler ist: " + ergebnis);
+
+                        teiler_anzahl++;
+
+                    }
+
+                }
+
+                if (teiler_anzahl == 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Die eingegebene Zahl ist eine Primzahl und nur durch 1 und sich selbst teilbar.");
+                    Console.WriteLine();
+                }
+
+                teiler_anzahl = 1;
+                Console.Write("Soll das Programm erneut ausgeführt werden? (j)");
+                antwort = Console.ReadLine();
+                Console.WriteLine();
+                
+                
+            }
+
+            while (antwort == "j");
+
+            Console.ReadKey();
+        }
+    }
+}
